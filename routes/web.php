f700df2c28abd10d63a83ebbc8eb670919ca2301
mhmdhkzn <162764096+mhmdhkzn@users.noreply.github.com>
2025-11-14 18:17:43 +0800

    zan

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('halaman home');
 });
 
-Route::get('/hallo', function () {
-    return view('zan');
+Route::get('/', function () {
+    return view('halaman about');
 });
