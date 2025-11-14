dc8aa1d6409b216626035e72e09abe6d3f430954
mhmdhkzn <162764096+mhmdhkzn@users.noreply.github.com>
2025-11-14 18:24:32 +0800

    bawe

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('halaman home');
-});
-
-Route::get('/', function () {
-    return view('halaman about');
-});
+Route::get('/home', function () {
+    return view('halaman_home');
+})->name('home');
